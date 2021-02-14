77652c365c7251e8862508cb7f4301969f80f2b7
Azenga Kevin <azenga.kevin7@gmail.com>
2021-02-14 03:53:32 +0300

    Initial Commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,8 @@
+<?php namespace App\Http\Controllers;
+
+class ProductsController{
+
+    public function __construct() {
+        var_dump("Products Controller");
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\ProductsController;
+
+require_once(__DIR__ . '/../bootstrap/app.php');
+
+new ProductsController();
