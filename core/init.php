c13c5e5cd5257314cd2718d8b0cf84e96d946406
Azenga Kevin <azenga.kevin7@gmail.com>
2021-02-14 11:36:49 +0300

    Wip, session start and loading of requird files

---
+++ core/init.php
@@ -0,0 +1,12 @@
+<?php
+
+    session_start();
+
+    require_once(__DIR__ . '/../config/db.php');
+    require_once(__DIR__ . '/../config/filesystem.php');
+    require_once(__DIR__ . '/../utils/helpers.php');
+    require_once(__DIR__ . '/../utils/db.php');
+
+    //if(!file_exists(__DIR__ . '/../public/uploads')) storageLink();
+    
+
